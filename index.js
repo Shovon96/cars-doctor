346352792b924b215755c94834b4ff99b20e431b
Shovon96 <fakhruddinshovon@gmail.com>
2023-10-27 22:05:33 +0600

    delete and petch added

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ async function run() {
         await client.connect();
 
         const servicesCollection = client.db('carsDoctor').collection('services');
+        const bookingsCollection = client.db('carsDoctor').collection('bookings');
 
         app.get('/services', async (req, res) => {
             const cursor = servicesCollection.find();
@@ -38,12 +39,48 @@ async function run() {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const options = {
-                projection: { title: 1, price: 1, service_id: 1 },
+                projection: { title: 1, price: 1, service_id: 1, img: 1 },
             };
             const result = await servicesCollection.findOne(query)
             res.send(result);
         })
 
+        // booking releted apis
+
+        app.get('/bookings', async(req, res) => {
+            let query = {};
+            if(req.query?.email){
+                query = { email: req.query.email}
+            }
+            const result = await bookingsCollection.find(query).toArray();
+            res.send(result)
+        })
+
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+            const result = await bookingsCollection.insertOne(booking);
+            res.send(result)
+        })
+
+        app.delete('/bookings/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await bookingsCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        app.patch('/bookings/:id', async(req, res) =>{
+            const id = req.body.id;
+            const filter = { _id: new ObjectId(id)};
+            const updatedBooking = req.body;
+            const updateDoc = {
+                $set: {
+                    status: updatedBooking.status
+                }
+            }
+            const result = await bookingsCollection.updateOne(filter, updateDoc);
+            res.send(result)
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
